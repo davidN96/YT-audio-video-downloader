90028c58c612b6a9bfe3304516ce5918d2ad0f5e
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-06-02 14:14:21 +0200

    [ VIEWS ] : Form valudation created in index.js

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+const urlForm = document.getElementById("videoForm");
+const videoUrlInput = document.querySelector(".urlInput");
+const errorInfo = document.querySelector(".errorInfo");
+
+urlForm.addEventListener("submit", (e) => {
+  const isEmpty = !videoUrlInput.value;
+  const isTooShort = videoUrlInput.value.length < 20;
+  const noHttps = !videoUrlInput.value.includes("https");
+  const noYoutube =
+    !videoUrlInput.value.includes("youtube") ||
+    !videoUrlInput.value.includes("youtu.be");
+
+  if (isEmpty || isTooShort || noHttps || noYoutube) {
+    e.preventDefault();
+    errorInfo.textContent = "Invalid URL address.";
+  }
+});
