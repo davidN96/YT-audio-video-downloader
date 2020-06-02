ce9b315fb1f8a7ea91d2de9b7a0279da9030f428
Dawid Nienajadło <dawidnienajadlo96@gmail.com>
2020-06-02 14:26:06 +0200

    [ ROUTER ] : /find route configured, connected with controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import findController from "../controllers/find.controller.js";
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
+
+router.get("/find", findController.getInfo);
 
 export default router;
